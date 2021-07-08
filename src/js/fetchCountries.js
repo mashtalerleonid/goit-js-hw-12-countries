8c622d2ea33d6444527c3f45613bbfcf999a9ade
mashtalerleonid <mashtalerleonid@gmail.com>
2021-07-08 13:24:34 +0300

    Отклонить при ok===false

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -3,10 +3,7 @@ const BASE_URL = 'https://restcountries.eu/rest/v2';
 function fetchCountries(name) {
   return fetch(`${BASE_URL}/name/${name}?fields=name;capital;population;flag;languages`).then(
     response => {
-      if (response.status === 404) {
-        return Promise.reject();
-      }
-      return response.json();
+      return response.ok ? response.json() : Promise.reject();
     },
   );
 }
